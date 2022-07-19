29af1ec01596fe0555ce06c405057ab3f0108fcb
niranjannlc <niranjannlc10@gmail.com>
2022-07-19 12:04:12 +0545

    adding app name in manifest file

---
+++ app/src/main/java/com/example/playwithui/di/ApplicationModule.kt
@@ -0,0 +1,10 @@
+package com.example.playwithui.di
+
+import android.app.Application
+import dagger.Module
+import dagger.hilt.InstallIn
+
+@Module
+@InstallIn(ApplicationComponet)
+class ApplicationModule {
+}
